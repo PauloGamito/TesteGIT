a94aad1df27142f2b6ef0bfce1f78bcb8d35cda2
PauloGamito <aluno@S08PC01.local>
2016-05-04 19:18:17 +0100

    Cube Translate Script

---
+++ Assets/SCRIPTS/CubeTranslate.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeTranslate : MonoBehaviour 
+{
+
+	//PUBLIC
+
+	public float lerpFactor;
+	public float destination = 5;
+	public float startPosX = 0;
+	public float endPosX = 5;
+
+	//PRIVATE
+
+	private Vector3 currentPosition = new Vector3();
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		lerpFactor += Time.deltaTime;
+		if (transform.position.x != destination) 
+		{
+			currentPosition.x = Mathf.Lerp (currentPosition.x, destination, lerpFactor);
+			transform.position = currentPosition;
+		}
+
+
+		else
+		{
+			if (transform.position.x == startPosX)
+				destination = endPosX;
+			else 
+				destination = startPosX;
+			lerpFactor = 0;
+		}
+	}
+}
